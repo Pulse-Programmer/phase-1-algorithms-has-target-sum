e1f01fcf4a7f425a0f503551e8ac18539ae10967
Stephen Ochieng <materiamystic@gmail.com>
2024-04-07 11:54:43 +0300

    Adds the sum functionality

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 1; i < array.length; i++) {
+    let sum = array[0] + array[i];
+    if (sum === target) {
+      return true;
+    }
+  }
+  if (array.length > 1) {
+    array.shift(); //Removes the first element from the array
+    return hasTargetSum(array, target); //recursive call to the altered array
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
+  O(n)
 */
 
 /*
-  Add written explanation of your solution here
+  Add your pseudocode here:
+
+  Add the first number in the array to all next elements in the array
+  Iteratively/recursively move one index step forward and add the next number to the rest of the array elements.
+  If sum of numbers is equal to target
+     return true
+
+  Return false if no sum matches the target
+*/
+
+/*
+  Add written explanation of your solution here:
+  The function loops across the array adding each array element to the other elements and checks if the sum
+  matches the target. If not it recursively calls itself to check if there is a match and returns false if there
+  is none.
 */
 
 // You can run `node index.js` to view these console logs
